1012deefcca8e6be6db4f96ce6262ee1166fd0a5
Washington <wash_muniz@yahoo.dk>
2022-08-19 23:20:31 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("A Aurora fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
